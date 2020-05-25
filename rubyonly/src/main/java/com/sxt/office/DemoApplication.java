0ac4f261c49ddb99d5796ac1ff4893ae67bbe639
RubyOnly <tanghua@111.com>
2020-05-25 14:24:43 +0800

    添加mapper注解，扫描

--- rubyonly/src/main/java/com/sxt/office/DemoApplication.java
+++ rubyonly/src/main/java/com/sxt/office/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.sxt.office;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.sxt.office.mapper"})
 public class DemoApplication {
 
     public static void main(String[] args) {
