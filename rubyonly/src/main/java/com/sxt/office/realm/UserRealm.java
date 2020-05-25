503f578513f7636b95b9830d10d029753a31a9e0
RubyOnly <tanghua@111.com>
2020-05-25 15:35:30 +0800

    user用户认证

--- rubyonly/src/main/java/com/sxt/office/realm/UserRealm.java
+++ rubyonly/src/main/java/com/sxt/office/realm/UserRealm.java
@@ -1,12 +1,17 @@
 package com.sxt.office.realm;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sxt.office.common.ActiverUser;
+import com.sxt.office.domain.User;
 import com.sxt.office.service.UserService;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -15,6 +20,15 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Version 1.0
  */
 public class UserRealm extends AuthorizingRealm {
+
+    /**
+     *  重写getName方法
+     * @return
+     */
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
 
     @Autowired
     private UserService userService;
@@ -31,12 +45,31 @@ public class UserRealm extends AuthorizingRealm {
 
     /**
      *  认证
-     * @param authenticationToken
+     * @param token
      * @return
      * @throws AuthenticationException
      */
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("loginname", token.getPrincipal().toString());
+
+        User user = userService.getOne(queryWrapper);
+
+        if (user != null) {
+
+            ActiverUser activerUser = new ActiverUser();
+
+            activerUser.setUser(user);
+
+            ByteSource credentialsSalt = ByteSource.Util.bytes(user.getSalt());
+
+            SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(activerUser, user.getPwd(), credentialsSalt, this.getName());
+
+            return info;
+        }
+
         return null;
     }
 }
