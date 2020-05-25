df38a8c44cfdf1acf14f4340c869dd8c9af01440
RubyOnly <tanghua@111.com>
2020-05-25 15:17:35 +0800

    创建UserRealm并继承认证授权realm

---
+++ rubyonly/src/main/java/com/sxt/office/realm/UserRealm.java
@@ -0,0 +1,42 @@
+package com.sxt.office.realm;
+
+import com.sxt.office.service.UserService;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Author tanghua
+ * @Date: 2020/5/25 15:12
+ * @Version 1.0
+ */
+public class UserRealm extends AuthorizingRealm {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     *  授权
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    /**
+     *  认证
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
