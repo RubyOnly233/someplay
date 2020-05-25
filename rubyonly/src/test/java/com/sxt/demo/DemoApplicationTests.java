0e9d0642546ee1be6096c5b15ed3df7a5edbb69b
RubyOnly <tanghua@111.com>
2020-05-25 10:50:43 +0800

    初始文件

---
+++ rubyonly/src/test/java/com/sxt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
