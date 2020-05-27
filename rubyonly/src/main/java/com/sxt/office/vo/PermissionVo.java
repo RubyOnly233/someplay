15f5a7b40fa68529cf933b540ba21f2df3e2a685
RubyOnly <tanghua@111.com>
2020-05-27 14:51:10 +0800

    包文件路径处理

---
+++ rubyonly/src/main/java/com/sxt/office/vo/PermissionVo.java
@@ -0,0 +1,18 @@
+package com.sxt.office.vo;
+
+import com.sxt.office.domain.Permission;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @Author tanghua
+ * @Date: 2020/5/26 16:56
+ * @Version 1.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class PermissionVo extends Permission {
+
+
+    private static final long serialVersionUID = 1497419355868462409L;
+}
