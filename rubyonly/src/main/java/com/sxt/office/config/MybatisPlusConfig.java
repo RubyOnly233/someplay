c5e66512d2060a671fa9009df1673a2d96ce87a3
RubyOnly <tanghua@111.com>
2020-05-28 14:32:05 +0800

    mybatisplus的配置类分页插件

---
+++ rubyonly/src/main/java/com/sxt/office/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.sxt.office.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatisplus的配置类
+ * @author LJH
+ *
+ */
+@Configuration
+@ConditionalOnClass(value= {PaginationInterceptor.class})
+public class MybatisPlusConfig {
+
+	/**
+	 *  分页插件
+	 * @return
+	 */
+	@Bean
+	public PaginationInterceptor  paginationInterceptor() {
+		return new PaginationInterceptor();
+	}
+
+}
