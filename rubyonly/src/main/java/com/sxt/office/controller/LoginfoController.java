6ffd42beed4480a78e7c8f5b4c1d2cee49f7a9d2
RubyOnly <tanghua@111.com>
2020-05-28 14:59:03 +0800

    日志全查询

--- rubyonly/src/main/java/com/sxt/office/controller/LoginfoController.java
+++ rubyonly/src/main/java/com/sxt/office/controller/LoginfoController.java
@@ -1,6 +1,15 @@
 package com.sxt.office.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sxt.office.common.DataGridView;
+import com.sxt.office.domain.Loginfo;
+import com.sxt.office.service.LoginfoService;
+import com.sxt.office.vo.LoginfoVo;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +25,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/loginfo")
 public class LoginfoController {
+
+    @Autowired
+    private LoginfoService loginfoService;
+
+    /**
+     *  全查询
+     * @return
+     */
+    @RequestMapping("/loadAllLoginfo")
+    private DataGridView loadAllLoginfo(LoginfoVo loginfoVo) {
+
+        IPage<Loginfo> page = new Page<>(loginfoVo.getPage(),loginfoVo.getLimit());
+
+        QueryWrapper<Loginfo> queryWrapper = new QueryWrapper<>();
+
+        queryWrapper.like(StringUtils.isNotBlank(loginfoVo.getLoginname()), "loginname", loginfoVo.getLoginname());
+
+        queryWrapper.like(StringUtils.isNotBlank(loginfoVo.getLoginip()), "loginip", loginfoVo.getLoginip());
+
+        queryWrapper.ge(loginfoVo.getStartTime() != null, "logintime", loginfoVo.getStartTime());
+
+        queryWrapper.le(loginfoVo.getEndTime() != null, "logintime", loginfoVo.getEndTime());
+
+        queryWrapper.orderByDesc("logintime");
+
+        loginfoService.page(page, queryWrapper);
+
+        return new DataGridView(page.getTotal(),page.getRecords());
+    }
 
 }
 
