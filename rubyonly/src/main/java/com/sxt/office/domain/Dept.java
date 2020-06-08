6483c4875a24716a423745d28c7f4b5d509c6b28
RubyOnly <tanghua@111.com>
2020-06-08 15:44:14 +0800

    时间类型修改

--- rubyonly/src/main/java/com/sxt/office/domain/Dept.java
+++ rubyonly/src/main/java/com/sxt/office/domain/Dept.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.time.LocalDateTime;
 import java.io.Serializable;
+import java.util.Date;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -48,7 +50,7 @@ public class Dept implements Serializable {
      */
     private Integer ordernum;
 
-    private LocalDateTime createtime;
+    private Date createtime;
 
 
 }
